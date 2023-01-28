842b8d043692eaaa13b3b1b2c2a27194382836e9
Ifan Fairuz <ifanfairuz@gmail.com>
2023-01-28 16:18:23 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,9 @@ module.exports = {
   apps: [
     {
       name: "inv-nduoseh",
-      script: "next start -p 3030",
+      script: "node_modules/next/dist/bin/next",
+      args: 'start -p 3030',
+      watch: false,
       error_file: "./logs/error.log",
       log_file: "./logs/log.log",
       time: true,
